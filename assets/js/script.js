f263edea1ac2b83f95d35583716c1110b2becdf8
jeansilvatech <jeanpsilva10@gmail.com>
2023-07-04 21:38:00 -0300

    [fix] resolução de problema de demora ao carregar os personagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,57 +1,22 @@
-
 const content = document.querySelector('.content')
 const loading = document.querySelector('.loading')
 const contentCards = document.querySelector('.content .characters')
-
 const btnHouse = document.querySelectorAll('.buttons button')
-const res = await fetch("https://hp-api.onrender.com/api/characters")
-const data = await res.json();
 let bgCard = ""
 let iconHouse = ""
-setInterval(() => {
-    loading.style.display = 'none'
-    content.style.display = 'flex'
-}, 8000);
-data.map( async(characters)=>{
-    
-    const imgDefault = "../assets/img/img-default.png"
-   
-        switch(characters.house){
-            case "Gryffindor":
-                iconHouse ="../assets/img/Gryffindor.webp"
-                bgCard = "#B22222"
-            break;
-            case "Slytherin":
-                iconHouse ="../assets/img/Slytherin.webp"
-                bgCard = "#228B22"
-            break;
-            case "Hufflepuff":
-                iconHouse ="../assets/img/Hufflepuff.webp"
-                bgCard = "#B8860B"
-            break;
-            case "Ravenclaw":
-                iconHouse ="../assets/img/Ravenclaw.webp"
-                bgCard = "#1E90FF"
-            break;
-            default:
-                iconHouse ="../assets/img/default.webp"
-                bgCard = "#1C1C1C"
-                break;
-    
-        }
-        contentCards.innerHTML+=`
-        <div class="card-character ${characters?.house}" style="background-color:${bgCard} ;">
-                <img src="${characters.image ===""?imgDefault:characters.image}" alt="" class="character-img">
-                <h2 class="name-character">${characters.name}</h2>
-                <p class="name-actor center"><span>${characters?.actor}</span></p>
-                <img src="${iconHouse}" class="house-character" title="${characters?.house}" alt="Icon house ${characters.house}">
-                <span>${characters?.house}</span>
-                </div>
-        `
-})
-const card = document.querySelectorAll('.card-character')
-btnHouse.forEach((btn)=>{
-    
+let data = [];
+async function api(){
+    try{
+    const res = await fetch("https://hp-api.onrender.com/api/characters")
+    data = await res.json();
+    render(data)
+    }catch(err){
+        console.log(err)
+    }
+}
+function btnRenderHouse(){
+    const card = document.querySelectorAll('.card-character')
+    btnHouse.forEach((btn)=>{
     switch(btn.name){
         case "Gryffindor":
             bgCard = "#B22222"
@@ -72,10 +37,10 @@ btnHouse.forEach((btn)=>{
     }
     btn.style.backgroundColor = bgCard
     btn.addEventListener('click', ()=>{
-       
         card.forEach((cardList)=>{
         const house =cardList.classList.contains(btn.name)
            if(house === true){
+            console.log("oi")
             cardList.style.display = 'flex'
            }else{
             cardList.style.display = 'none'
@@ -84,4 +49,48 @@ btnHouse.forEach((btn)=>{
         })
     })
 })
+}
+
+let render = (dataCharacters)=>{
+    const responseApi = dataCharacters.map((characters)=>{
+    
+        const imgDefault = "../assets/img/img-default.png"
+       
+            switch(characters.house){
+                case "Gryffindor":
+                    iconHouse ="../assets/img/Gryffindor.webp"
+                    bgCard = "#B22222"
+                break;
+                case "Slytherin":
+                    iconHouse ="../assets/img/Slytherin.webp"
+                    bgCard = "#228B22"
+                break;
+                case "Hufflepuff":
+                    iconHouse ="../assets/img/Hufflepuff.webp"
+                    bgCard = "#B8860B"
+                break;
+                case "Ravenclaw":
+                    iconHouse ="../assets/img/Ravenclaw.webp"
+                    bgCard = "#1E90FF"
+                break;
+                default:
+                    iconHouse ="../assets/img/default.webp"
+                    bgCard = "#1C1C1C"
+                    break;
+            }
+            return `
+            <div class="card-character ${characters?.house}" style="background-color:${bgCard} ;">
+                <img src="${characters.image ===""?imgDefault:characters.image}" alt="" class="character-img">
+                <h2 class="name-character">${characters.name}</h2>
+                <p class="name-actor center"><span>${characters?.actor}</span></p>
+                <img src="${iconHouse}" class="house-character" title="${characters?.house}" alt="Icon house ${characters.house}">
+                <span>${characters?.house}</span>
+            </div>
+            `
+    }).join("")
+    contentCards.innerHTML = responseApi;
+    btnRenderHouse()
+}
+api()
+
 
